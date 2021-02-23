b2730599b6c9587657ec1df6c24ab290fc1dd976
shuvo1998 <sk.ray1998t@gmail.com>
2021-02-23 23:15:43 +0600

    started

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Shuvo Kumer Ray First React app <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
